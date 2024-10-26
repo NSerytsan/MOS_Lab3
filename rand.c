ac9236b6ebad5527460d9556e99559049cd168cd
NSerytsan <nserytsan@gmail.com>
2024-10-26 15:12:21 +0300

    rand implementation

--- rand.c
+++ rand.c
@@ -1,4 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "common.h"
 int main(int argc, char **argv)
 {
+    bench_args args;
+    get_bench_args(&args, argc, argv);
+
+    array_t array = init_array(args.array_sz, 1);
+    if (array == NULL)
+        sys_error("Error allocating array");
+
+    bench_results results;
+    results.sum = 0;
+    results.start = now_us();
+
+    for (size_t i = 0; i < args.array_sz; i++)
+    {
+        const size_t ri = rand() % args.array_sz;
+        results.sum += array[ri];
+    }
+
+    results.end = now_us();
+    evaluate_benchmark(&results, &args, NULL);
+
+    free(array);
+
     return 0;
 }
